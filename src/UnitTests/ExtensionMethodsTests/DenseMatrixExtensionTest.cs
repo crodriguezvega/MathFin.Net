c2effb175fcec33f2ff70e0150da544426a6cb76
crodriguezvega <crodveg@yahoo.es>
2014-04-02 08:44:38 +0200

    Fixed warning.

--- src/UnitTests/ExtensionMethodsTests/DenseMatrixExtensionTest.cs
+++ src/UnitTests/ExtensionMethodsTests/DenseMatrixExtensionTest.cs
@@ -27,7 +27,7 @@ namespace UnitTests.ExtensionMethodsTests
       var option = new EuropeanCall(1, 10);
       var parameters = new DiscretizationParameters(0, 20, 1, 1);
 
-      var V = new DenseMatrix(new double[,] { { 1, 2 }, { 3, 4 } });
+      var V = DenseMatrix.OfArray(new double[,] { { 1, 2 }, { 3, 4 } });
       var val = V.PriceAt(0.25, 5, option, parameters);
 
       Assert.AreEqual(1.75, val);
